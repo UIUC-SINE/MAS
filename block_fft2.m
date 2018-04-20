158eeac877700b71c68fd957aae888053d3d10fe
ukamaci <kamaci.ulas@gmail.com>
2018-04-20 16:10:13 -0500

    block_fft2.m debugged

--- block_fft2.m
+++ block_fft2.m
@@ -1,4 +1,4 @@
-% by Fatih Çaðatay Akyön & Ulaþ Kamacý 2017
+% by Fatih ï¿½aï¿½atay Akyï¿½n & Ulaï¿½ Kamacï¿½ 2017
 % fatihcagatayakyon@gmail.com & kamaci.ulas@gmail.com
 
 function fft_x=block_fft2(x,a,aa)
@@ -21,7 +21,7 @@ else
     for i=1:k
         for j=1:p
             y=indexer(x,i,j,a);
-            y=padarray(y,[(aa/2-(size(y,1)-1)/2) (aa/2-(size(y,2)-1)/2)]);
+            y=padarray(y,[(aa/2-ceil((size(y,1)-1)/2)) (aa/2-ceil((size(y,2)-1)/2))]);
             y=y(1:aa,1:aa);
             y=fftshift(fft2(ifftshift(y)));
             fft_x(1+(i-1)*aa:i*aa, 1+(j-1)*aa:j*aa) = y;
