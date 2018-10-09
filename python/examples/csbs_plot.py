b3d628e4e05a44168f0024bcafa81d7fa313dfe6
evan <evan@evanw.org>
2018-10-08 19:32:52 -0500

    make example plotting functions use circ_psf

--- python/examples/csbs_plot.py
+++ python/examples/csbs_plot.py
@@ -4,17 +4,18 @@
 
 from mas.csbs import csbs
 from mas import sse_cost
-from mas.psf_generator import generate_measurements
+from mas.psf_generator import generate_measurements, circ_incoherent_psf
 from mas.plotting import fourier_slices
 import numpy as np
 
 # initialize A matrix
-measurements = generate_measurements(source_wavelengths=np.array([32.0e-9,  33.4e-9, 35.0e-9]),
-                                     image_width=501,
+measurements = generate_measurements(source_wavelengths=np.array([33.4e-9, 33.7e-9, 33.8e-9]),
+                                     psf_generator=circ_incoherent_psf,
+                                     image_width=51,
                                      num_copies=10)
 
 # run CSBS (modifies measurements in place)
-csbs(measurements, sse_cost, 10, lam=100)
+csbs(measurements, sse_cost, 10, lam=20)
 
 # plot results
 plt = fourier_slices(measurements)
--- python/examples/lambda_selection.py
+++ python/examples/lambda_selection.py
@@ -3,17 +3,24 @@
 # plot final csbs result for a range of lambdas
 from mas.csbs import csbs
 from mas import sse_cost
-from mas.psf_generator import generate_measurements
+from mas.psf_generator import generate_measurements, circ_incoherent_psf
 import numpy as np
 from matplotlib import pyplot as plt
+import copy
 
 
 # run csbs algorithm on a range of lambdas
-lambdas = np.logspace(-1, 2, 10)
+lambdas = np.logspace(-10, 2, 20)
 copies = []
+orig_measurements = generate_measurements(
+    source_wavelengths=np.array([33.4e-9, 33.7e-9, 33.8e-9]),
+    measurement_wavelengths=5,
+    image_width=51,
+    psf_generator=circ_incoherent_psf,
+)
 for lam in lambdas:
     print('----------------------', lam)
-    measurements = generate_measurements(source_wavelengths=np.array([33.4e-9, 33.7e-9, 33.8e-9]), image_width=51)
+    measurements = copy.deepcopy(orig_measurements)
     csbs(measurements, sse_cost, 10, lam=lam)
     copies.append(measurements.copies)
 
