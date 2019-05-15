ce42d917fde09cb92c1192cbc5be61a8b01507e7
evan <evan@evanw.org>
2019-05-15 11:21:49 -0500

    remove get_measurements mode kwarg

--- python/mas/forward_model.py
+++ python/mas/forward_model.py
@@ -84,7 +84,7 @@ def rectangle_adder(*, image, size, upperleft):
     ] += np.ones(size)
     return image
 
-def get_measurements(real=False, *, sources, psfs, meas_size=None, mode='circular'):
+def get_measurements(real=False, *, sources, psfs, meas_size=None):
     """
     Convolve the sources and psfs to obtain measurements.
     Args:
@@ -106,69 +106,35 @@ def get_measurements(real=False, *, sources, psfs, meas_size=None, mode='circula
     [k,p,ss,ss] = psfs.psfs.shape
     ta, tb = [aa + ss - 1, bb + ss - 1]
 
-    if mode == 'linear':
-        raise NotImplemented
-    #     psfs.selected_psfs = np.zeros((k,p,ta,tb))
-    #     sources_r = size_equalizer(sources, [ta,tb])
-    #     selected_psfs = size_equalizer(psfs.psfs, [ta,tb])
-    #     psfs.selected_psfs = size_equalizer(psfs.psfs, meas_size)
 
-    #     selected_psfs = np.repeat(selected_psfs, psfs.copies.astype(int), axis=0)
-    #     selected_psf_dfts = np.fft.fft2(selected_psfs)
-    #     psfs.selected_psfs = np.repeat(psfs.selected_psfs, psfs.copies.astype(int), axis=0)
-    #     psfs.selected_psf_dfts = np.fft.fft2(psfs.selected_psfs)
-    #     psfs.selected_psf_dfts_h = block_herm(psfs.selected_psf_dfts)
-    #     psfs.selected_GAM = block_mul(
-    #         psfs.selected_psf_dfts_h,
-    #         psfs.selected_psf_dfts
-    #     )
+    # FIXME: make it work for 2D input, remove selected_psfs
+    # FIXME: ;move psf_dft computation to PSFs (make PSFs accept sampling_interval and o
+    # output size arguments)
 
-    #     # ----- forward -----
-    #     return np.fft.ifftshift(
-    #         size_equalizer(
-    #             np.fft.fftshift(
-    #                 np.real(
-    #                     np.fft.ifft2(
-    #                         block_mul(
-    #                             selected_psf_dfts,
-    #                             np.fft.fft2(sources_r)
-    #                         )
-    #                     )
-    #                 ), axes=(2,3)
-    #             ), meas_size
-    #         ), axes=(2,3)
-    #     )
+    psfs.selected_psfs = np.zeros((k,p,aa,bb))
 
+    # reshape psfs
+    psfs.selected_psfs = size_equalizer(psfs.psfs, ref_size=[aa,bb])
 
-    elif mode == 'circular':
-        # FIXME: make it work for 2D input, remove selected_psfs
-        # FIXME: ;move psf_dft computation to PSFs (make PSFs accept sampling_interval and o
-        # output size arguments)
+    psfs.selected_psfs = np.repeat(psfs.selected_psfs, psfs.copies.astype(int), axis=0)
+    psfs.selected_psf_dfts = np.fft.fft2(psfs.selected_psfs)
+    psfs.selected_psf_dfts_h = block_herm(psfs.selected_psf_dfts)
+    psfs.selected_GAM = block_mul(
+        psfs.selected_psf_dfts_h,
+        psfs.selected_psf_dfts
+    )
 
-        psfs.selected_psfs = np.zeros((k,p,aa,bb))
-
-        # reshape psfs
-        psfs.selected_psfs = size_equalizer(psfs.psfs, ref_size=[aa,bb])
-
-        psfs.selected_psfs = np.repeat(psfs.selected_psfs, psfs.copies.astype(int), axis=0)
-        psfs.selected_psf_dfts = np.fft.fft2(psfs.selected_psfs)
-        psfs.selected_psf_dfts_h = block_herm(psfs.selected_psf_dfts)
-        psfs.selected_GAM = block_mul(
-            psfs.selected_psf_dfts_h,
-            psfs.selected_psf_dfts
-        )
-
-        # ----- forward -----
-        measurement = np.fft.fftshift(
-            np.fft.ifft2(
-                block_mul(
-                    psfs.selected_psf_dfts,
-                    np.fft.fft2(sources)
-                )
-            ),
-            axes=(2, 3)
-        )
-        return measurement.real if real else measurement
+    # ----- forward -----
+    measurement = np.fft.fftshift(
+        np.fft.ifft2(
+            block_mul(
+                psfs.selected_psf_dfts,
+                np.fft.fft2(sources)
+            )
+        ),
+        axes=(2, 3)
+    )
+    return measurement.real if real else measurement
 
 
 def get_contributions(real=False, *, sources, psfs):
