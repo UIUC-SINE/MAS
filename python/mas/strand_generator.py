02bf200e2dae5980a29549ac9559813a7e1f8eb3
ukamaci <kamaci.ulas@gmail.com>
2019-05-17 23:07:42 -0500

    update the strand generator and the strands data

--- python/mas/strand_generator.py
+++ python/mas/strand_generator.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2, skimage
 from scipy.sparse import coo_matrix
+from scipy.ndimage.filters import gaussian_filter
 
-def strand(theta, x, thickness=7, intensity=1, image_width=160):
+def strand(theta, x, thickness=22, intensity=1, image_width=512):
     """Generate single strand image"""
 
     pt1 = (x + int(image_width * np.tan(np.deg2rad(theta))), 0) # point where it crosses y=0 line
@@ -11,7 +12,7 @@ def strand(theta, x, thickness=7, intensity=1, image_width=160):
     img = np.zeros((image_width, image_width))
     return cv2.line(img, pt1, pt2, intensity, thickness=thickness)
 
-def strands(num_strands=100, thickness=7, max_angle=30,
+def strands(num_strands=100, thickness=22, max_angle=20,
             image_width=160, initial_width=512):
     """Generate nanoflare image
 
@@ -25,17 +26,18 @@ def strands(num_strands=100, thickness=7, max_angle=30,
     Returns:
         ndarray: (image_width, image_width) image containing strands"""
 
-    img = np.zeros((image_width,image_width))
+    img = np.zeros((initial_width,initial_width))
     for _ in range(num_strands):
         theta = np.random.uniform(-max_angle, max_angle)
         x  = np.random.randint(
-            0 if theta < 0 else image_width * -np.tan(np.deg2rad(theta)),
-            image_width - image_width * np.tan(np.deg2rad(theta)) if theta < 0 else image_width
+            0 if theta < 0 else initial_width * -np.tan(np.deg2rad(theta)),
+            initial_width - initial_width * np.tan(np.deg2rad(theta)) if theta < 0 else initial_width
         )
         intensity = np.random.rand()
 
-        img += strand(theta, x, thickness, intensity, image_width)
+        img += strand(theta, x, thickness, intensity, initial_width)
 
-    imgr = skimage.transform.resize(img, (image_width, image_width))
+    imgr = gaussian_filter(img, sigma=2)
+    imgr = skimage.transform.resize(imgr, (image_width, image_width))
     imgr = imgr / imgr.max()
     return imgr
