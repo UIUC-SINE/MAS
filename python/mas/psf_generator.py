972e30f943038d8a8161c418f656d45b2a27db36
evan <evan@evanw.org>
2019-05-22 20:28:32 -0500

    fix typo

--- python/mas/psf_generator.py
+++ python/mas/psf_generator.py
@@ -284,7 +284,6 @@ class PSFs():
             approx_start = sieve.diameter * sieve.smallest_hole_diameter / (max(focal_lengths) + 10 * max(dofs))
             approx_end = sieve.diameter * sieve.smallest_hole_diameter / (min(focal_lengths) - 10 * min(dofs))
             measurement_wavelengths = np.linspace(approx_start, approx_end, measurement_wavelengths)
-            measurement_wavlengths = np.insert(
             measurement_wavelengths = np.insert(
                 measurement_wavelengths,
                 np.searchsorted(measurement_wavelengths, source_wavelengths),
