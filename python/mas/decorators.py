a95df0c42409f9867045e3502a4f03fa79a9f471
evan <evan@evanw.org>
2019-09-04 13:31:41 -0500

    add _vectorize examples

--- python/mas/decorators.py
+++ python/mas/decorators.py
@@ -8,6 +8,29 @@ def _vectorize(signature='(m,n)->(i,j)', included=[0]):
     Args:
         signature (str): override mapping behavior
         included (list): list of ints and strs of position/keyword arguments to iterate over
+
+    Returns:
+        function: decorator which can be applied to nonvectorized functions
+
+    Signature examples:
+
+        signature='(),()->()', included=[0, 1]
+            first two arguments to function are vectors. Loop through each
+            element pair in these two vectors and put the result in a vector of
+            the same size. e.g. if args 0 and 1 are of size (5, 5), the output
+            from the decorated function will be a vector of size (5, 5)
+
+        signature='(m,n)->(i,j)', included=[0]
+            input argument is a vector with at least two dimensions. Loop
+            through all 2d vectors (using last 2 dimensions) in this input
+            vector, process each, and return a 2d vector for each. e.g. if arg
+            0 is a vector of size (10, 5, 5), loop through each (5, 5) vector
+            and return a (10, 5, 5) vector of all the results
+
+        signature='(m,n)->()', included=[0]
+            input argument is a vector with at least two dimensions. Loop
+            through each 2d image and return a 1d vector. e.g. if arg 0 is a
+            vector of size (10, 5, 5), return a vector of size (10)
     """
     def decorator(func):
 
