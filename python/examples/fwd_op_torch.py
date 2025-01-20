487dd94e0b519995567d7188e9ce14b372287b15
ukamaci <kamaci.ulas@gmail.com>
2025-01-20 10:39:32 -0600

    list fwd_op_torch args and add noise in the example code

--- python/examples/fwd_op_torch.py
+++ python/examples/fwd_op_torch.py
@@ -1,6 +1,7 @@
 from mas.psf_generator import PSFs, PhotonSieve
 from mas.strand_generator import strands
 from mas.forward_model import size_equalizer, get_fwd_op_torch
+from slitless.forward import add_noise
 from scipy.ndimage.filters import gaussian_filter
 from scipy.signal import convolve2d
 from mas.tracking import mb_kernel
@@ -8,9 +9,34 @@ from skimage.transform import resize, rescale
 from torch.nn.functional import conv2d
 
 x = strands(image_width=128)
-fwd_op, psf_final = get_fwd_op_torch()
 
-y = fwd_op(x)
+diameter=75e-3 # meters
+smallest_hole_diameter=16e-6 # meters
+wavelengths=np.array([30.4e-9]) # meters
+plane_offset=15e-3 # meters
+drift_angle=-45 # degrees
+drift_velocity=0.10e-3 # meters / s
+jitter_rms=3e-6 # meters
+frame_rate=7.5 # Hz
+pixel_size=7e-6 # meters
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+fwd_op, psf_final = get_fwd_op_torch(
+    diameter=diameter,
+    smallest_hole_diameter=smallest_hole_diameter,
+    wavelengths=wavelengths,
+    plane_offset=plane_offset,
+    drift_angle=drift_angle,
+    drift_velocity=drift_velocity,
+    jitter_rms=jitter_rms,
+    frame_rate=frame_rate,
+    pixel_size=pixel_size,
+    device=device
+)
+
+meas_noiseless = fwd_op(x)
+
+meas = add_noise(meas_noiseless, dbsnr=10, noise_model='Poisson')
 
 plt.figure()
 plt.imshow(x, cmap='hot')
@@ -19,9 +45,15 @@ plt.title('Input Image')
 plt.show()
 
 plt.figure()
-plt.imshow(y.squeeze().cpu().numpy(), cmap='hot')
+plt.imshow(meas_noiseless.squeeze().cpu().numpy(), cmap='hot')
 plt.colorbar()
 plt.title('Blurred Image')
+plt.show()
+
+plt.figure()
+plt.imshow(meas.squeeze().cpu().numpy(), cmap='hot')
+plt.colorbar()
+plt.title('Blurred Noisy Image')
 plt.show()
 
 plt.figure()
